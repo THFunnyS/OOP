c7aaf23e603e03278790904994d4710ff1d97aa4
THFunnyS <115404967+THFunnyS@users.noreply.github.com>
2023-10-04 22:02:54 +0400

    Update Main.java
    Добавление класса CompositeFunction

--- LR2/src/Main.java
+++ LR2/src/Main.java
@@ -4,5 +4,10 @@ public class Main {
         IdentityFunction func=new IdentityFunction();
         double y=func.apply(12.5);
         System.out.print(y);
+        System.out.println();
+
+        CompositeFunction func2=new CompositeFunction();
+        double z= func2.apply(51);
+        System.out.print(z);
     }
 }
