6a5cd1ee15d8236c7ac68d365724f95a8aea3e95
THFunnyS <115404967+THFunnyS@users.noreply.github.com>
2023-10-06 13:06:01 +0400

    Update Main.java

--- LR2/src/Main.java
+++ LR2/src/Main.java
@@ -1,13 +1,6 @@
 import functions.*;
 public class Main {
     public static void main(String args[]){
-        IdentityFunction func=new IdentityFunction();
-        double y=func.apply(12.5);
-        System.out.print(y);
-        System.out.println();
-
-        CompositeFunction func2=new CompositeFunction();
-        double z= func2.apply(51);
-        System.out.print(z);
+        
     }
 }
