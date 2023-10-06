5c77eac25a75c3db7f641bc3c7717dfa3415096c
SwedenLord <145365180+SwedenLord@users.noreply.github.com>
2023-10-06 17:07:59 +0400

    Add files via upload
    Composite+test

--- LR2/src/functions/CompositeFunction.java
+++ LR2/src/functions/CompositeFunction.java
@@ -3,12 +3,14 @@ package functions;
 public class  CompositeFunction implements MathFunction{
     private MathFunction firstFunction, secondFunction;
 
-    public CompositeFunction(MathFunction firstFunction, MathFunction secondFunction){
+    public CompositeFunction(MathFunction firstFunction, MathFunction secondFunction)
+    {
         this.firstFunction=firstFunction;
         this.secondFunction=secondFunction;
     }
 
-    public double apply(double x){
+    public double apply(double x)
+    {
         return secondFunction.apply(firstFunction.apply(x));
     }
 }
