8f5d06190e473346418eeac358a713cfe64713b3
THFunnyS <115404967+THFunnyS@users.noreply.github.com>
2023-10-07 21:35:48 +0400

    Update AbstractTabulatedFunction.java

--- LR2/src/functions/AbstractTabulatedFunction.java
+++ LR2/src/functions/AbstractTabulatedFunction.java
@@ -1,26 +1,11 @@
 package functions;
 
 public abstract class AbstractTabulatedFunction implements TabulatedFunction {
-    protected int count;
     abstract protected int floorIndexOfX(double x);
     abstract protected double extrapolateLeft(double x);
     abstract protected double extrapolateRight(double x);
 
-    abstract protected double interpolate(double x, int floorIndex);
-    protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY);
-    {
-        return leftY + (rightY - leftY) / (rightX - leftX) * (x - leftX);
-    }
-    public double apply(double x){
-        double interpolate;
-        if(x<getX(0)) interpolate = this.extrapolateLeft(x);
-        else if(x>getX(count-1))interpolate = this.extrapolateRight(x);
-        else{
-            int indexOfX = indexOfX(x);
-            if(indexOfX>0) interpolate = this.getY(indexOfX);
-            else interpolate = this.interpolate(x, floorIndexOfX(x));
-        }
-        return interpolate;
-    }
-
+    protected abstract double interpolate(double x, int floorIndex);
+    protected abstract double interpolate(double x, double leftX, double rightX, double leftY, double rightY);
+    public abstract double apply(double x);
 }
