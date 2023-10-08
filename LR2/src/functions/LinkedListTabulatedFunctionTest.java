074c9f591f1dad6341e8c3f5eea57324c2c838bb
THFunnyS <115404967+THFunnyS@users.noreply.github.com>
2023-10-08 18:45:19 +0400

    Update LinkedListTabulatedFunctionTest.java

--- LR2/src/functions/LinkedListTabulatedFunctionTest.java
+++ LR2/src/functions/LinkedListTabulatedFunctionTest.java
@@ -7,7 +7,7 @@ import junit.framework.Assert;
 public class LinkedListTabulatedFunctionTest {
     double[] arr1={1,2,3};
     double[] arr2={4,5,6};
-    TabulatedFunction testLink=new LinkedListTabulatedFunction(arr1,arr2);
+    LinkedListTabulatedFunction testLink=new LinkedListTabulatedFunction(arr1,arr2);
     @Test
     public void testgetCount(){
         Assert.assertEquals(3,testLink.getCount());
