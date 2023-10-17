df291fe6f305386f54b6968fdf28d5d02f9e5e0e
THFunnyS <115404967+THFunnyS@users.noreply.github.com>
2023-10-17 18:29:12 +0400

    Delete LR2/src/functions directory

--- LR2/src/functions/Integral.java
+++
@@ -1,8 +0,0 @@
-package functions;
-
-public class Integral implements MathFunction{
-
-    public double apply(double x){
-        return (((Math.pow(x,3))/3)-((Math.pow(0,3))/3));
-    }
-}
--- LR2/src/functions/ZeroFunctionTest.java
+++
@@ -1,8 +0,0 @@
-package functions;
-
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.Test;
-import junit.framework.Assert;
-public class ZeroFunctionTest {
-
-}
