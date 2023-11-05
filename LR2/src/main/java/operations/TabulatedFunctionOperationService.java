e949f9df8f494cf9a7f6764d101641ec9f69b623
SwedenLord <gastankpro@gmail.com>
2023-11-06 03:03:53 +0400

    lab 4 task 2.2

--- LR2/src/main/java/operations/TabulatedFunctionOperationService.java
+++ LR2/src/main/java/operations/TabulatedFunctionOperationService.java
@@ -1,9 +1,46 @@
 package operations;
 
+import exceptions.InconsistentFunctionsException;
 import functions.Point;
 import functions.TabulatedFunction;
+import functions.factory.ArrayTabulatedFunctionFactory;
+import functions.factory.TabulatedFunctionFactory;
 
 public class TabulatedFunctionOperationService {
+    protected TabulatedFunctionFactory factory;
+    public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    public TabulatedFunctionOperationService() {
+        this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    public void setFactory(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    public TabulatedFunctionFactory getFactory() {
+        return this.factory;
+    }
+    TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation) {
+
+        if (a.getCount() != b.getCount()) throw new InconsistentFunctionsException();
+        else {
+            Point[] arraysA = asPoints(a);
+            Point[] arraysB = asPoints(b);
+
+            double[] xValue = new double[a.getCount()];
+            double[] yValue = new double[a.getCount()];
+
+            for (int i = 0; i < a.getCount(); i++) {
+                if (arraysA[i].x == arraysB[i].x) xValue[i] = arraysA[i].x;
+                else throw new InconsistentFunctionsException();
+                yValue[i] = operation.apply(arraysA[i].y, arraysB[i].y);
+            }
+            return factory.create(xValue, yValue);
+        }
+    }
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
 
         Point[] asPointsArray = new Point[tabulatedFunction.getCount()];
@@ -14,4 +51,15 @@ public class TabulatedFunctionOperationService {
         }
         return asPointsArray;
     }
+    public TabulatedFunction add(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, Double::sum);
+    }
+
+    public TabulatedFunction subtraction(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u - v);
+    }
+    private interface BiOperation {
+        double apply(double u, double v);
+    }
+
 }
--- LR2/src/test/java/operations/TabulatedFunctionOperationServiceTest.java
+++ LR2/src/test/java/operations/TabulatedFunctionOperationServiceTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import functions.ArrayTabulatedFunction;
 import functions.LinkedListTabulatedFunction;
 import functions.Point;
+import functions.TabulatedFunction;
+import functions.factory.LinkedListTabulatedFunctionFactory;
+import functions.factory.TabulatedFunctionFactory;
 import operations.TabulatedFunctionOperationService;
 import org.junit.jupiter.api.Test;
 
@@ -15,9 +18,9 @@ class TabulatedFunctionOperationServiceTest {
     double[] yValue1 = {2, 3, 4, 5, 6};
     double[] yValue2 = {8, 9, 10, 11, 12};
     ArrayTabulatedFunction func1 = new ArrayTabulatedFunction(xValue, yValue1);
-    ArrayTabulatedFunction func2 = new ArrayTabulatedFunction(xValue,yValue2);
+    ArrayTabulatedFunction func2 = new ArrayTabulatedFunction(xValue, yValue2);
     LinkedListTabulatedFunction func3 = new LinkedListTabulatedFunction(xValue, yValue1);
-    LinkedListTabulatedFunction func4 = new LinkedListTabulatedFunction(xValue,yValue2);
+    LinkedListTabulatedFunction func4 = new LinkedListTabulatedFunction(xValue, yValue2);
     TabulatedFunctionOperationService tabulatedFunctionOperationService = new TabulatedFunctionOperationService();
 
     @Test
@@ -31,4 +34,49 @@ class TabulatedFunctionOperationServiceTest {
             ++i;
         }
     }
+    TabulatedFunctionFactory factory1 = new LinkedListTabulatedFunctionFactory();
+    TabulatedFunctionOperationService operation1 = new TabulatedFunctionOperationService(factory1);
+    TabulatedFunctionOperationService operation2 = new TabulatedFunctionOperationService();
+
+    @Test
+    public void addTest() {
+
+        TabulatedFunction result1 = operation1.add(func1, func2);
+        for (int i = 0; i < result1.getCount(); i++) {
+            assertEquals(yValue1[i] + yValue2[i], result1.getY(i));
+        }
+
+        TabulatedFunction result2 = operation2.add(func3, func4);
+        for (int i = 0; i < result2.getCount(); i++) {
+            assertEquals(yValue1[i] + yValue2[i], result2.getY(i));
+        }
+
+
+        TabulatedFunction result3 = operation2.add(func1, func3);
+        for (int i = 0; i < result3.getCount(); i++) {
+            assertEquals(yValue1[i] + yValue2[i], result3.getY(i));
+        }
+
+
+    }
+
+    @Test
+    void subtractionTest() {
+        TabulatedFunction result1 = operation1.subtraction(func1, func2);
+        for (int i = 0; i < result1.getCount(); i++) {
+            assertEquals(yValue1[i] - yValue2[i], result1.getY(i));
+        }
+
+        TabulatedFunction result2 = operation2.subtraction(func4, func3);
+        for (int i = 0; i < result2.getCount(); i++) {
+            assertEquals(yValue1[i] - yValue2[i], result2.getY(i));
+        }
+
+
+        TabulatedFunction result3 = operation2.subtraction(func1, func3);
+        for (int i = 0; i < result3.getCount(); i++) {
+            assertEquals(yValue1[i] - yValue2[i], result3.getY(i));
+        }
+    }
+
 }
