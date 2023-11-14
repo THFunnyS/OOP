dda6fa18d4104dff2eec0c8968e49ad5de2bb981
6104PMI <pmi6x04@mail.ru>
2023-11-14 13:08:03 +0400

    Lab5 task2-3(x)

--- LR2/src/main/java/functions/ArcSumFunction.java
+++
@@ -1,8 +0,0 @@
-package functions;
-
-public class ArcSumFunction implements MathFunction{
-    @Override
-    public double apply(double x){
-        return (Math.asin(x)+Math.acos(x));
-    }
-}
--- LR2/src/main/java/functions/factory/ArrayTabulatedFunctionFactory.java
+++
@@ -1,9 +0,0 @@
-package functions.factory;
-
-import functions.ArrayTabulatedFunction;
-import functions.TabulatedFunction;
-public class ArrayTabulatedFunctionFactory implements TabulatedFunctionFactory {
-    public TabulatedFunction create(double[] xValues, double[] yValues) {
-        return new ArrayTabulatedFunction(xValues, yValues);
-    }
-}
